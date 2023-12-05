f64903a6c061d6a2cc50477f913d4bc1d68d0c82
abdulaziz1717 <abdulaziznurhakim777@gmail.com>
2023-12-05 21:56:14 +0700

    weapon leveling system

--- Assets/Scripts/Weapons/WeaponScriptableObject.cs
+++ Assets/Scripts/Weapons/WeaponScriptableObject.cs
@@ -25,4 +25,13 @@ public class WeaponScriptableObject : ScriptableObject
     [SerializeField]
     int pierce;
     public int Pierce { get => pierce; private set => pierce = value; }
+
+    [SerializeField]
+    int level;  //Not meant to be modified in game [Only in Editor]
+    public int Level { get => level; private set => level = value; }
+
+    [SerializeField]
+    GameObject nextLevelPrefab;  //The prefab of the next level i.e. what the object becomes when it levels up
+                                 //Not to be confused with the prefab to be spawned at the next level
+    public GameObject NextLevelPrefab { get => nextLevelPrefab; private set => nextLevelPrefab = value; }
 }
