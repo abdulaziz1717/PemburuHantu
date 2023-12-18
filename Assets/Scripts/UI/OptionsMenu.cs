b594ec7475defb45d0706570581a2b61d32e742d
nirmaela <nirmaelax@gmail.com>
2023-12-18 13:06:19 +0700

    atur slider volume
    slider volume ada di main menu sama pause screen

--- Assets/Scripts/UI/OptionsMenu.cs
+++ Assets/Scripts/UI/OptionsMenu.cs
@@ -8,19 +8,28 @@ using UnityEngine.UI;
 public class OptionsMenu : MonoBehaviour
 {
     [SerializeField] public AudioMixer mainMixer;
-    [SerializeField] public Slider volumeSlider = null;
+    [SerializeField] public Slider volumeSlider;
+    public float volume = 1f;
+
+    public const string keyVolume = "VOLUME";
 
     private void Start()
     {
+        float previousVolume = PlayerPrefs.GetFloat(keyVolume);
+        SetVolume(previousVolume);
+        volumeSlider.value = previousVolume;
         //LoadValues();
     }
 
     public void SetVolume(float sliderValue)
     {
+        volume = sliderValue;
         mainMixer.SetFloat("MasterVolume", Mathf.Log10(sliderValue) * 20);
+
+        PlayerPrefs.SetFloat(keyVolume, sliderValue);
     }
 
-    public void SaveVolumeButton()
+    /*public void SaveVolumeButton()
     {
         float volumeValue = volumeSlider.value;
         PlayerPrefs.SetFloat("VolumeValue", volumeValue);
@@ -32,7 +41,9 @@ public class OptionsMenu : MonoBehaviour
         float volumeValue = PlayerPrefs.GetFloat("VulumeValue");
         volumeSlider.value = volumeValue;
         AudioListener.volume = volumeValue;
-    }
+    }*/
+
+    //----------------SET FULLSCREEN-------------------------//
 
     public void SetFullscreen (bool isFullscreen)
     {
